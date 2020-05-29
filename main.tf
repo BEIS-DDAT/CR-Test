a3e4e5a2442b90f0b6aa3f2d102387599579e789
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-05-29 13:24:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg01 " {
+resource "azurerm_resource_group" "rg01" {
   name     = "crtestrg01"
   location = "UKSouth"
 }
