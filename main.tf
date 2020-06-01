00dd246c7c84b21772ee185c82c0a2512bd26b0f
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-06-01 13:54:45 +0100

    Update main.tf
    Added tags and VNET

--- main.tf
+++ main.tf
@@ -5,4 +5,18 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg01" {
   name     = "crtestrg01"
   location = "UKSouth"
+  tags = {
+    environment = "CR TFE Demo"
+  }
 }
+
+resource "azure_virtual_network" "vnet01" {
+  name = "crtestvnet01"
+  address_space = ["10.100.0.0/20"]
+  location = "UKSouth"
+  resource_group_name = "azure_resource_group.rg01.name"
+  tags = {
+    environment = "CR TFE Demo"
+  }
+}
+  
