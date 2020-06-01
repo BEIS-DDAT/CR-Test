01a01e4ed21c5cbfea7f510855f39c430a312c4d
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-06-01 16:38:07 +0100

    Update main.tf
    added subnet 01

--- main.tf
+++ main.tf
@@ -20,4 +20,14 @@ resource "azurerm_virtual_network" "vnet01" {
     environment = "CR TFE Demo"
   }
 }
+
+resource "azurerm_subnet" "subnet01" {
+  name = "CRTestSN01"
+  resource_group_name = azurerm_resource_group.rg01.name
+  virtual_network_name = azurerm_virtual_network.vnet01.name
+  address_prefix = "10.100.1.0/24"
+  tags = {
+    environment = "CR TFE Demo"
+  }
+}
   
