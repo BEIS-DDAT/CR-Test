9e53abc7ab79d2b620ae954d49d2930f00e45f65
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-06-01 16:24:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ resource "azurerm_resource_group" "rg01" {
   }
 }
 
-resource "azure_virtual_network" "vnet01" {
+resource "azurerm_virtual_network" "vnet01" {
   name = "crtestvnet01"
   address_space = ["10.100.0.0/20"]
   location = "UKSouth"
-  resource_group_name = "azure_resource_group.rg01.name"
+  resource_group_name = "azurerm_resource_group.rg01.name"
   tags = {
     environment = "CR TFE Demo"
   }
