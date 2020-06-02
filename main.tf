d127023cb13211497d9f0f41fed158214e2055ec
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-06-02 10:54:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,3 +83,37 @@ resource "azurerm_network_interface_security_group_association" "NicNSG01" {
     network_interface_id      = azurerm_network_interface.NIC01.id
     network_security_group_id = azurerm_network_security_group.nsg01.id
 }
+
+resource "azurerm_linux_virtual_machine" "vm01" {
+    name                  = "CRTFETestVm01"
+    location              = "UKSouth"
+    resource_group_name   = azurerm_resource_group.rg01.name
+    network_interface_ids = [azurerm_network_interface.NIC01.id]
+    size                  = "Standard_B2s"
+
+    os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04.0-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "CRTFETestVM01"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+        
+    admin_ssh_key {
+        username       = "azureuser"
+        public_key     = var.ssh_public_key
+    }
+
+    tags = {
+        environment = "CR TFE Demo"
+    }
+}
