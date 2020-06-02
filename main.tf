58ddbe2585086ef7b11573565eaca1edda55c1c9
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-06-02 10:01:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,4 +27,15 @@ resource "azurerm_subnet" "subnet01" {
   virtual_network_name = azurerm_virtual_network.vnet01.name
   address_prefix = "10.100.1.0/24"
 }
+
+resource "azurerm_public_ip" "CRTFEpip01" {
+    name                         = "CRTestPIP01"
+    location                     = "UKSouth"
+    resource_group_name          = azurerm_resource_group.rg01.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "CR TFE Demo"
+    }
+}
   
