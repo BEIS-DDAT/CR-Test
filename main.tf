87f80047d04a7ed73159049f30c309433cea717a
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-06-02 10:55:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "azurerm" {
   version = "2.0"
   features {}
 }
+//--------------------------------------------------------------------
+// Variables
+variable "ssh_public_key" {}
+//--------------------------------------------------------------------
+
 
 resource "azurerm_resource_group" "rg01" {
   name     = "crtestrg01"
